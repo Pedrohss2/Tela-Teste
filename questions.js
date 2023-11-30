2908e4a26c8b64174b978afef0864398bde7b8af
pedroH <pedro2266809ph@gmial.com>
2023-11-30 08:57:29 -0300

    Add question

---
+++ questions.js
@@ -0,0 +1,48 @@
+export default [
+  {
+    question: "Qual o resultado de 32 x 8?",
+    answers: [
+      { option: "256", correct: true },
+      { option: "453", correct: false },
+      { option: "254", correct: false },
+      { option: "200", correct: false }
+    ],
+  },
+  {
+    question:  "Qual o valor satisfaz essa conta 8 x ? =  72",
+    answers: [
+      { option: "9", correct: true },
+      { option: "11", correct: false },
+      { option: "7", correct: false },
+      { option: "6", correct: false }
+    ],
+  },
+  {
+    question:"Realizar a conta sem o auxílio da calculadore: 925 - 413?",
+    answers: [
+      { option: "412", correct: false },
+      { option: "321", correct: false },
+      { option: "512", correct: true },
+      { option: "222", correct: false },
+    ],
+  },
+  {
+    question: "Qual a raiz quadrada de 256?",
+    answers: [
+      { option: "17", correct: false },
+      { option: "16", correct: true },
+      { option: "15", correct: false },
+      { option: "44", correct: false },
+    ],
+  },
+  {
+    question: "Qual é a área de um quadrado de 5cm de lado?",
+    answers: [
+      { option: "Não sei", correct: false },
+      { option: "25cm", correct: false },
+      { option: "25cm2", correct: true },
+      { option: "25", correct: false },
+    ],
+  },
+
+];
