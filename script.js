6883daf055b8f13eb2ec7c09490a3af74bfc7f27
Pedro Henrique <116673382+Pedrohss2@users.noreply.github.com>
2023-11-30 10:44:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function finish() {
   textFinish.innerHTML = `você acertou ${questionsCorrect} de ${questions.length}`;
   content.style.display = "none";
   contentFinish.style.display = "flex";
-  pontos = 0;
+  pontos.innerHTML = 0;
 }
 
 function loadQuestion() {
